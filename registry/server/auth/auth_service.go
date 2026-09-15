93232aef28638fb9c205f9d5d0623715e359796a
Arsalan <arsalan@Arsalans-MacBook-Air.local>
2026-09-16 01:58:28 +0530

    added status and codes for grpc services

--- registry/server/auth/auth_service.go
+++ registry/server/auth/auth_service.go
@@ -10,6 +10,8 @@ import (
 	"github.com/uddinArsalan/ferry-registry/adapters/token"
 	genauth "github.com/uddinArsalan/ferry-registry/proto/auth"
 	"github.com/uddinArsalan/ferry-registry/repository"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 type AuthService struct {
@@ -20,12 +22,28 @@ type AuthService struct {
 }
 
 var (
-	ErrInvalidCredentials = errors.New("invalid credentials")
-	ErrUnauthorized       = errors.New("unauthorized request")
-	ErrUserNotFound       = errors.New("user not found")
-	ErrInvalidInput       = errors.New("invalid input")
-	AccessTokenTTL        = 15 * time.Minute
-	RefreshTokenTTL       = 7 * 24 * time.Hour
+	ErrInvalidCredentials = status.Error(
+		codes.Unauthenticated,
+		"invalid crendetials",
+	)
+	ErrUnauthenticated = status.Error(
+		codes.Unauthenticated,
+		"unauthenticated",
+	)
+	ErrUserNotFound = status.Error(
+		codes.NotFound,
+		"user not found",
+	)
+	ErrInternalServer = status.Error(
+		codes.Internal,
+		"internal server error",
+	)
+	ErrInvalidInput = status.Error(
+		codes.InvalidArgument,
+		"invalid input",
+	)
+	AccessTokenTTL  = 15 * time.Minute
+	RefreshTokenTTL = 7 * 24 * time.Hour
 )
 
 func NewAuthService(authRepo *repository.AuthRepo, passwordStore password.PasswordStore, tokenStore token.TokenStore) *AuthService {
@@ -38,24 +56,24 @@ func NewAuthService(authRepo *repository.AuthRepo, passwordStore password.Passwo
 
 func (a *AuthService) Register(ctx context.Context, req *genauth.RegisterRequest) (*genauth.RegisterResponse, error) {
 	if req.Name == "" || req.Email == "" {
-		return nil, ErrInvalidInput
+		return nil, ErrInvalidCredentials
 	}
 	hash, err := a.passwordStore.HashAndEncodePassword(req.Password)
 	// need to consider something for error
 	if err != nil {
-		return nil, err
+		return nil, ErrInternalServer
 	}
 	userID, err := a.authRepo.CreateUser(ctx, req.Name, req.Email, hash)
 	if err != nil {
-		return nil, err
+		return nil, ErrInternalServer
 	}
 	accessToken, err := a.tokenStore.GenerateToken(userID, AccessTokenTTL)
 	if err != nil {
-		return nil, err
+		return nil, ErrInternalServer
 	}
 	refreshToken, err := a.tokenStore.GenerateRefreshToken()
 	if err != nil {
-		return nil, err
+		return nil, ErrInternalServer
 	}
 	now := time.Now()
 
@@ -63,7 +81,7 @@ func (a *AuthService) Register(ctx context.Context, req *genauth.RegisterRequest
 	refreshTokenExpiresAt := now.Add(RefreshTokenTTL)
 
 	if err = a.authRepo.CreateRefreshToken(ctx, userID, a.tokenStore.HashToken(refreshToken), refreshTokenExpiresAt); err != nil {
-		return nil, err
+		return nil, ErrInternalServer
 	}
 	return &genauth.RegisterResponse{
 		AccessToken:      accessToken,
@@ -81,15 +99,13 @@ func (a *AuthService) Login(ctx context.Context, req *genauth.LoginRequest) (*ge
 	if err != nil {
 		if errors.Is(err, sql.ErrNoRows) {
 			return nil, ErrUserNotFound
-			// need to do something here so ferry client knows this user doesnt exist
-			// and show a message to user to create instead
 		}
-		return nil, err
+		return nil, ErrInternalServer
 	}
 	//compare password
 	match, err := a.passwordStore.ComparePasswordAndHash(req.Password, user.PasswordHash)
 	if err != nil {
-		return nil, err
+		return nil, ErrInternalServer
 	}
 	if !match {
 		return nil, ErrInvalidCredentials
@@ -97,11 +113,11 @@ func (a *AuthService) Login(ctx context.Context, req *genauth.LoginRequest) (*ge
 
 	accessToken, err := a.tokenStore.GenerateToken(user.ID, AccessTokenTTL)
 	if err != nil {
-		return nil, err
+		return nil, ErrInternalServer
 	}
 	refreshToken, err := a.tokenStore.GenerateRefreshToken()
 	if err != nil {
-		return nil, err
+		return nil, ErrInternalServer
 	}
 	now := time.Now()
 
@@ -109,7 +125,7 @@ func (a *AuthService) Login(ctx context.Context, req *genauth.LoginRequest) (*ge
 	refreshTokenExpiresAt := now.Add(RefreshTokenTTL)
 
 	if err = a.authRepo.CreateRefreshToken(ctx, user.ID, a.tokenStore.HashToken(refreshToken), refreshTokenExpiresAt); err != nil {
-		return nil, err
+		return nil, ErrInternalServer
 	}
 	return &genauth.LoginResponse{
 		AccessToken:      accessToken,
@@ -121,25 +137,25 @@ func (a *AuthService) Login(ctx context.Context, req *genauth.LoginRequest) (*ge
 
 func (a *AuthService) Refresh(ctx context.Context, req *genauth.RefreshRequest) (*genauth.RefreshResponse, error) {
 	if req.RefreshToken == "" {
-		return nil, ErrUnauthorized
+		return nil, ErrInvalidInput
 	}
 	oldToken, err := a.authRepo.GetRefreshToken(ctx, a.tokenStore.HashToken(req.RefreshToken))
 	if err != nil {
-		return nil, err
+		return nil, ErrInternalServer
 	}
 	if oldToken.RevokedAt != nil {
-		return nil, ErrUnauthorized
+		return nil, ErrUnauthenticated
 	}
 	if time.Now().After(oldToken.ExpiresAt) {
-		return nil, ErrUnauthorized
+		return nil, ErrUnauthenticated
 	}
 	accessToken, err := a.tokenStore.GenerateToken(oldToken.UserID, AccessTokenTTL)
 	if err != nil {
-		return nil, err
+		return nil, ErrInternalServer
 	}
 	newToken, err := a.tokenStore.GenerateRefreshToken()
 	if err != nil {
-		return nil, err
+		return nil, ErrInternalServer
 	}
 	now := time.Now()
 
@@ -147,7 +163,7 @@ func (a *AuthService) Refresh(ctx context.Context, req *genauth.RefreshRequest) 
 	refreshTokenExpiresAt := now.Add(RefreshTokenTTL)
 
 	if err = a.authRepo.CreateAndUpdateRefreshToken(ctx, oldToken.ID, a.tokenStore.HashToken(newToken), oldToken.UserID, refreshTokenExpiresAt); err != nil {
-		return nil, err
+		return nil, ErrInternalServer
 	}
 	return &genauth.RefreshResponse{
 		AccessToken:      accessToken,
